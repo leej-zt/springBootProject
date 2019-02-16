2bf778f9508cb92969f0774ec826738fcd0581f2
leeji <leeji@DESKTOP-2PBBT8B>
2019-02-16 20:32:00 +0900

    20190216 画面のテスト leej
    テスト画面の表示テスト

---
+++ pj1/src/main/java/com/example/pj1/web/TestController.java
@@ -0,0 +1,15 @@
+package com.example.pj1.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String testController() {
+		return "TestView";
+	}
+}
