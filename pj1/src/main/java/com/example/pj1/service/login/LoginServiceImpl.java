be167f8e61ff6c301c114a716d5c8d9f339a2f51
leej-zt <lee.jieun.jp@gmail.com>
2019-08-31 00:03:01 +0900

    20190831 ログイン画面修正 leej

--- pj1/src/main/java/com/example/pj1/service/login/LoginServiceImpl.java
+++ pj1/src/main/java/com/example/pj1/service/login/LoginServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.pj1.service.login;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,10 @@ public class LoginServiceImpl implements LoginService {
             UsernamePasswordToken token =
                     new UsernamePasswordToken(userId, password);
             currentUser.login(token);
+
+            Session session = currentUser.getSession();
+            session.setAttribute("userId", userId);
+
         } catch (AuthenticationException ae) {
             return false;
         } catch (Exception e) {
